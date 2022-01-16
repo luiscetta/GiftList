7d397e4fc2ae9e27fe5b21dbaf80cc5e5b6d2947
luiscetta <filipe.cetta@gmail.com>
2022-01-16 08:02:49 -0300

    refactoring gift list css code

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className="header-container">
             <div className="header-content">
-                <a className="nav-link" href="/">Home</a>
+                <a className="nav-link" href="/">Página Inicial</a>
                 <a className="nav-link" href="/gift-list">Lista de Presentes</a>
                 <a className="nav-link" href="/presence">Confirme sua presença</a>
                 <a className="nav-link" href="/contact">Contato</a>
--- src/Pages/Address/index.js
+++ src/Pages/Address/index.js
@@ -1,8 +1,19 @@
+import './styles.css';
+
 export default function Address() {
     return (
         <div className="add-container">
             <div className="add-content">
-                <h2>Local do Evento</h2>
+                <h2 className="add-title">Local do Evento</h2>
+                <div className="maps">
+
+                </div>
+                <div className="add-section">
+                    <h3 className="section-title">Endereço:</h3>
+                    <p className="section-paragraph">Rua Floriano Manoel da Fonseca, Nº 34, Sagrado Coração, Paciência</p>
+                    <h3 className="section-title">Ponto de Referência:</h3>
+                    <p className="section-paragraph">Em frente a Escola Municipal Franklin Távora</p>
+                </div>
             </div>
         </div>
     );
